bf83bafff4328484fbad97d4b1dabe917b55de88
“Polina” <prykova007@gmail.com>
2024-05-27 15:58:54 +0300

    Restore config directory

---
+++ src/main/java/org/example/config/ConfigLoader.java
@@ -0,0 +1,25 @@
+package org.example.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigLoader {
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find application.properties");
+            } else {
+                properties.load(input);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
